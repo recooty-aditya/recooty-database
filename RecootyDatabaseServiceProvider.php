bd6850d16e71ad3bdd5afb97853beeb2ddca263f
Aditya Rungta <aditya@recooty.com>
2024-03-15 15:56:04 +0530

    added provider

---
+++ RecootyDatabaseServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Recooty;
+
+use Illuminate\Support\ServiceProvider;
+
+class RecootyDatabaseServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/resources/migrations' => database_path('migrations'),
+        ], 'migrations');
+    }
+
+    public function register()
+    {
+        // Register any package services
+    }
+}
